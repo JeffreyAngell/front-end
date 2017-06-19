cb2370e6d13638e29e2b41fa57b8092db1fdfd4d
Jeffrey Angell <jeffreya@qualtrics.com>
2017-06-18 17:28:08 -0700

    Clean up a bit

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default props => {console.log(props); return (
+export default props => (
 	<select onChange={props.updateValue}>
 		{props.options.map((option, index) => {
 			return <option key={option.key} value={option.value}>{option.key}</option>
 		})}
 	</select>
-)};
+);
